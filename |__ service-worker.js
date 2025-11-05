f9f3c2d92978b8b81eda398069908cc8949f0b5e
sahil_rajheti <sahilkhan701766@gmail.com>
2025-11-05 13:43:08 +0530

    Create |__ service-worker.js

---
+++ |__ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "calc-cache-v1";
+const FILES_TO_CACHE = [
+  "/",           // root
+  // index.html is requested by browser; GitHub Pages serves /index.html for /
+  // add icons if you uploaded them
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
